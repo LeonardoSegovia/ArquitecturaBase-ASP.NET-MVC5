348f41c0222ed6873f0e472f3222d952c52c4d16
Segovia, Leonardo G <segovia.leonardo.gustavo@gmail.com>
2018-06-08 23:11:01 -0300

    Add project files.

---
+++ 04-Common/Common/DependecyFactory.cs
@@ -0,0 +1,13 @@
+﻿using LightInject;
+
+namespace Common
+{
+    public class DependecyFactory
+    {
+        public static T GetInstance<T>()
+        {
+            return new ServiceContainer()
+                                  .GetInstance<T>();
+        }
+    }
+}
